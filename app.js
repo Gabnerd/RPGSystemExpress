384035debc2e2d5c0b99c7960fcb6df811728e8b
Gabnerd <ShaickMiles@gmail.com>
2021-05-19 10:10:55 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var indexRouter = require('./routes/index');
 var mestreRouter = require('./routes/mestre');
 var jogadorRouter = require('./routes/jogador');
 var socket = require('socket.io');
+var cors = require("cors");
 
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -14,11 +16,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 app.use("/", indexRouter);
 app.use("/mestre", mestreRouter);
 app.use("/jogador", jogadorRouter);
